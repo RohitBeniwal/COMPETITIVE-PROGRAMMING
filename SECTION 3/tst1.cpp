6ee8f888800b0c8d13a52a466ca8199954441d1c
RohitBeniwal <beniwalrb177@gmail.com>
2022-11-05 21:45:56 +0530

    s

---
+++ SECTION 3/tst1.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define vi vector<int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define ff first
+#define ss second
+typedef long long int ll;
+
+using namespace std;
+
+int main()
+{
+ios::sync_with_stdio(0);
+cin.tie(0);
+    cout<<"Rohit"<<endl;
+    return 0;
+}
